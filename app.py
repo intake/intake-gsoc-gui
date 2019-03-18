3b6745a488ee171164c68738137606c806b9722f
Martin Durant <martin.durant@utoronto.ca>
2019-03-18 16:21:38 -0400

    more typo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ pn.extension()
 
 
 def make_volume():
-    """Thie function makes a 3D array of data, a brain scan"""
+    """This function makes a 3D array of data, a brain scan"""
     from skimage import io
     vol = io.imread(
         "https://s3.amazonaws.com/assets.datacamp.com/blog_assets"
